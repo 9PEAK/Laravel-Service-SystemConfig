d0d56bc65e118e342dd3e6de9bd3e91c7f3ff0ae
Kane <52653784@qq.com>
2018-07-31 19:05:53 +0800

    Start

---
+++ src/Service/SystemConfig/Provider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service\System;
+
+use Illuminate\Support\ServiceProvider;
+
+class Provider extends ServiceProvider
+{
+
+
+	public function boot()
+	{
+		$this->migrate();
+		$this->config();
+	}
+
+	protected function migrate()
+	{
+
+//		return $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+
+		$this->publishes([
+			__DIR__.'/publish/migration.php' => database_path('migrations/9PeakLaravelServiceSystemConfig.php'),
+		]/*, 'passport-migrations'*/);
+	}
+
+	protected function config()
+	{
+		$this->publishes(
+			[
+				__DIR__.'/public/config.php' => config_path('system.php'),
+			],
+			'config'
+		);
+
+//		$path = realpath(__DIR__.'/../config/config.php');
+//		$this->publishes([$path => config_path('storage.php')], 'config');
+//		$this->mergeConfigFrom($path, 'storage');
+	}
+
+
+}
